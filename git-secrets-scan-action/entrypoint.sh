1c4431551055bae9f02a3d75c06083775e771365
Ojas Joshi <ojasjosh@amazon.com>
2020-10-09 01:15:44 -0700

    Check if GITHUB_WORKSPACE is set and repo is checked-out

--- git-secrets-scan-action/entrypoint.sh
+++ git-secrets-scan-action/entrypoint.sh
@@ -6,7 +6,19 @@ set -e
 cd ${HOME}
 git clone https://github.com/awslabs/git-secrets.git && cd git-secrets && make install 
 
+# Check if GITHUB_WORKSPACE is set
+if [[ -z "${DEPLOY_ENV}" ]]
+    echo "Required env variable GITHUB_WORKSPACE not set."
+    exit 1
+fi
+
 cd ${GITHUB_WORKSPACE}
+
+# Check if repository is checked-out using actions/checkout
+if [ -z "$(ls -A ) ."]; then
+    echo "Workspace is empty. Did you checkout the repository using actions/checkout?"
+    exit 1
+fi
 
 # Register git-secrets in the workspace repo
 git secrets --install
