ef419d3510dd962c8cc7b3ae991499466dd7ba9e
chengke <chengke@uefee1b00be3a55.ant.amazon.com>
2021-06-10 13:13:55 -0700

    pass in --install-ros

--- robomaker-sample-app-ci/src/aws-robomaker-sample-application-ci.ts
+++ robomaker-sample-app-ci/src/aws-robomaker-sample-application-ci.ts
@@ -114,7 +114,7 @@ async function fetchRosinstallDependencies(): Promise<string[]> {
       await exec.exec("bash", ["-c", `ln -snf /usr/share/zoneinfo/${timezone} /etc/localtime`]);
       await exec.exec("bash" , ["-c", `echo ${timezone} > /etc/timezone`]);
     }
-    await exec.exec("bash", ["-c", "scripts/setup.sh"]);
+    await exec.exec("bash", ["-c", `scripts/setup.sh --install-ros ${ROS_DISTRO}`]);
     loadROSEnvVariables();
     await exec.exec("apt-get", ["update"]);
     //zip required for prepare_sources step.
